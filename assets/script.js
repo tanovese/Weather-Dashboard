b96a216b40dd86b4ade594e0ca8e8982861ce814
tanovese <82059748+tanovese@users.noreply.github.com>
2023-05-21 15:10:50 -0400

    City input is working

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,25 @@ var cityKey= "42653fcad7f3685f0cfbf5d990ef0d44"
 var searchButton= document.getElementById("get-weather-button");
 const cityUrl= "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
 const defaultUrl= "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+var inputEl= document.getElementById("search-bar");
+// var city = inputEl.value;
+// console.log(city);
 
 searchButton.addEventListener("click", getWeather)
 
-function getWeather() {
-    var city= element.getTextContent()
+function getWeather(event) {
+    event.preventDefault();
+    // var city= element.getTextContent()
+    var city = inputEl.value;
+    console.log(city);
 
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q={New York},{state code},{country code}&limit={limit}&appid={cityKey}')
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={42653fcad7f3685f0cfbf5d990ef0d44}')
     .then(response => response.json())
-    console.log('http://api.openweathermap.org/geo/1.0/direct?q={New York},{state code},{country code}&limit={limit}&appid={cityKey}')
+    console.log('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={42653fcad7f3685f0cfbf5d990ef0d44}')
     .then(data => {
         var lat = data.lat;
         var lon = data.lon;
-        return fetch(`api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={defaultKey}`)
+        return fetch(`api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={a53947f76a99feda746fa882e7d4ef45}`)
     })
     .then(response => response.json())
     .then(data => {
